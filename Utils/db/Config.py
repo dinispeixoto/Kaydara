9ec8013c30d6540220f0a8a384cda41c2c097721
Ricardo Pereira <ricardo.agp.914@gmail.com>
2018-04-10 22:49:36 +0100

    add the database connection and an example

---
+++ Utils/db/Config.py
@@ -0,0 +1,15 @@
+import os
+import urllib.parse as urlparse
+from configparser import ConfigParser
+
+def config(filename='database.ini', section='postgresql'):
+    db = {} 
+
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    db['database'] = url.path[1:]
+    db['user'] = url.username
+    db['password'] = url.password
+    db['host'] = url.hostname
+    db['port'] = url.port
+
+    return db
---
+++ Utils/db/Database.py
@@ -0,0 +1,26 @@
+import os
+import Config
+import psycopg2
+
+class Database():
+    def __init__(self):
+        try:    
+            params = Config.config()
+            self.conn = psycopg2.connect(**params)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        self.cur = self.conn.cursor()
+
+    def query(self, query):
+        self.cur.execute(query)
+
+    def query_params(self, query, params):
+        self.cur.execute(query, params)
+
+    def result_set(self):
+        return self.cur.fetchall()
+
+    def close(self):
+        self.cur.close()
+        self.conn.commit()
+        self.conn.close()

775c94da69ee17989db16c5c4ba92f775fc3a151
marcelolima3 <marcelo-lima96@hotmail.com>
2018-04-14 22:55:40 +0100

    changed nlp workspace

--- src/NLP/NewsNLP.py
+++ src/NLP/NewsNLP.py
@@ -1,4 +1,3 @@
-from src.APIs import IBMWatsonAPI
 from src.APIs import FacebookAPI
 from src.APIs import NewsAPI
 from src.MsgBuilder import NewsMB
@@ -9,18 +8,19 @@ import json
 def process_message(results, cli):
     print('NEWS REQUEST')
     (intents, entities, newContext, output) = results
-    
+
     json_newContext = json.dumps(newContext, indent=2)
     Client.update_client_context(cli.id, json_newContext)
 
     for m in output:
-        FacebookAPI.send_message(cli.id, m)
-        if m == 'I got you!':
+        if m == '[OK] Process request':
             send_news(newContext, cli)
+        elif m == '':
+            FacebookAPI.send_message(cli.id, m)
 
 
 def send_news(context, cli):
-    news = NewsAPI.getTopHeadlines(context['topic'])
+    news = NewsAPI.getTopHeadlines(context['keyword'])
     elements = NewsMB.generateNewsPosts(news)
     FacebookAPI.send_carousel(cli.id, elements)
     Client.update_client_context(cli.id, None)
